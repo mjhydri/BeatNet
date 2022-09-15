5d0cb583b7069b745e5331915b9049b7b8fb2e88
mjhydri <mj.hydri@gmail.com>
2022-09-15 11:48:37 -0400

    Dependencies conflict fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import distutils.cmd
 # Required packages
 REQUIRED_PACKAGES = [
     'numpy',
-    'Cython',
+    'cython',
     'librosa>=0.8.0',
     'numba==0.54.1', # Manually specified here as librosa incorrectly states that it is compatible with the latest version of numba although 0.50.0 is not compatible. 
     'scipy',
